336e8dad5051bcbb0b74445f8edd246f9dc48e62
BaQuna <imbaquna@gmail.com>
2023-07-12 18:13:45 +0600

    finished homework js3

---
+++ script.js
@@ -0,0 +1,114 @@
+
+// function checkUser(user){
+//     if(user==='Baktygul'){
+//         return 'user admin true welcome, Baktygul'
+//     } else{
+//         return ' false not correct user'
+//     }
+// }
+// console.log(checkUser('Baktygul'))
+
+
+// function february(day) {
+//     if (day<='28'){
+//         return `today is ${day} of february`
+//     }
+//     else {
+//         return `on february only 28 day, your day is ${day} of another month`
+//     }
+//
+// }
+// console.log(february(29))
+
+
+// function equal(num){
+//     if (num===50){
+//         return `${num} is equals of 50`
+//     } else {
+//         return `${num} is not correct`
+//     }
+// }
+// console.log(equal(50))
+
+
+// function greetinUser(user,age){
+//     if(`${user}` && age>=18){
+//         return `${user} ${age} old, welcome`
+//     }else {
+//         return `${user} ${age} old, rejected`
+//     }
+// }
+// console.log(greetinUser('Alex', 19))
+
+
+// function checkLength(user) {
+//    if (user.length < 3) {
+//        return `${user } name is too short (min 3)`
+//    }
+//    if (user.length > 5) {
+//        return `${user }name is too long (max 5)`
+//     } else {
+//        return `${user } name is accepted`
+//     }
+// }
+// console.log(checkLength('ffff'))
+
+
+//
+// function checkNumber(age){
+//     if ( typeof age === 'string'){
+//         return `"${age}" must be a number`
+//     }
+//     if (age<20){
+//         return 'age must be over 20'
+//     }
+//     else{
+//         return 'correct'
+//     }
+//
+// }
+// console.log(checkNumber('ff'))
+
+
+
+// function checkString(age){
+//     if(typeof age==='number'){
+//         return `"${age}" must be a string`
+//     }
+//     if(age<23){
+//         return `${age } must be older then 23`
+//     }
+//     else {
+//         return 'correct'
+//     }
+// }
+// console.log(checkString('23'))
+
+
+// function checkDay(day) {
+//     if (day<=0 || day>7) {
+//         return 'max days of weeks 7'
+//         }else if(day===1){
+//             return 'today is monday'
+//         }else if(day===2){
+//             return 'today is tuesday'
+//         }else if(day===3){
+//             return 'today is wednesday'
+//         }else if (day===4){
+//             return 'today is thursday'
+//         }else if(day===5){
+//             return 'today is friday'
+//         }else if (day===6 || day===7){
+//             return 'today is weekend'
+//         }
+//     }
+// console.log(checkDay(7))
+
+
+
+
+
+
+
+
+
