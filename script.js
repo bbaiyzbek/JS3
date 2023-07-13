31214abcd5ad3880a471d9adfe1e0be0e9e2cce1
BaQuna <imbaquna@gmail.com>
2023-07-13 18:13:01 +0600

    added homework 4

--- script.js
+++ script.js
@@ -32,7 +32,7 @@
 
 
 // function greetinUser(user,age){
-//     if(`${user}` && age>=18){
+//     if(age>=18){
 //         return `${user} ${age} old, welcome`
 //     }else {
 //         return `${user} ${age} old, rejected`
@@ -103,6 +103,83 @@
 //         }
 //     }
 // console.log(checkDay(7))
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+/////// 1 \\\\\\\\
+
+// function checkWord(str){
+//     str=str.trim()
+//     str=str.slice(str.indexOf(' ')+1)+' '+str.slice(0, str.indexOf(' '))
+//     str=str.slice(0, str.lastIndexOf('o'))+'a'
+//     return `${str}`
+// }
+// console.log(checkWord(' hello world '))
+
+//////// 2 \\\\\\\\\
+
+// function checkLength(str){
+//     if(str.length % 2 === 0) {
+//         return `${str.length} (четное)`
+//     }else{
+//         return `${str.length} (не четное)`
+//     }
+// }
+// console.log(checkLength('eggg'))
+
+//////// 3 \\\\\\\\
+
+// function checkSpace(str){
+//     str = str.replaceAll(' ','')
+//     str = str[0].toUpperCase()+str
+//         .slice(1,5)+str[5]
+//         .toUpperCase()+str
+//         .slice(6,10)+str[10]
+//         .toUpperCase()+str
+//         .slice(11,15)+str[15]
+//         .toUpperCase()+str
+//         .slice(16,18)+str[18]
+//         .toUpperCase()+str
+//         .slice(19,22)
+//     return `${str}`
+// }
+// console.log(checkSpace('lorem ipsum dolor sit amet'))
+
+////////// 4 \\\\\\\\
+
+// function priceDiscount(price, discount){
+//     price=price-(price*discount/100)
+//     return `${price}`
+// }
+// console.log(priceDiscount(500,10))
+
+/////////// 5 \\\\\\\\
+
+// function checkString(str) {
+//     if (str.length !== 0) {
+//         if (typeof str === 'number') {
+//             return 'must be a string'
+//         } else if (str.length >= 4 && str.length <= 10) {
+//             if (str.length % 2 === 0) {
+//                 return `${str}`
+//             }else{
+//                 return 'odd number'
+//             }
+//         }else{
+//             return 'string must match in length'
+//         }
+//     } else {
+//         return 'must not be empty'
+//     }
+// }
+// console.log(checkString('fgfg'))
+
+
+
+
+
+
+
 
 
 
